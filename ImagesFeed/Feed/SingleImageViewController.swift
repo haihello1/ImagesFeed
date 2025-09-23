70e757b3ee6cff4f72b91568e72469126530a7e9
Chel Pchelov <danmigunov@gmail.com>
2025-09-23 17:51:09 +0300

    rewrite ui using ai

--- ImagesFeed/Feed/SingleImageViewController.swift
+++ ImagesFeed/Feed/SingleImageViewController.swift
@@ -1,3 +1,166 @@
 import UIKit
 
+final class SingleImageViewController: UIViewController {
+    
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded, let image else { return }
+            
+            imageView.image = image
+            imageView.frame.size = image.size
+            rescaleAndCenterImageInScrollView(image: image)
+        }
+    }
+    
+    private let scrollView: UIScrollView
+    private let imageView: UIImageView
+    private let backButton: UIButton
+    private let shareButton: UIButton
+    
+    convenience init(image: UIImage) {
+        self.init()
+        self.image = image
+    }
+    
+    init() {
+        scrollView = UIScrollView()
+        imageView = UIImageView()
+        backButton = UIButton(type: .system)
+        shareButton = UIButton(type: .system)
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        scrollView = UIScrollView()
+        imageView = UIImageView()
+        backButton = UIButton(type: .system)
+        shareButton = UIButton(type: .system)
+        super.init()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = AppColors.background
+        
+        setupScrollView()
+        setupImageView()
+        setupButtons()
+        
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        scrollView.delegate = self
+        
+        guard let image else { return }
+        imageView.image = image
+        imageView.frame.size = image.size
+        rescaleAndCenterImageInScrollView(image: image)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        shareButton.layer.cornerRadius = shareButton.frame.height / 2
+    }
+    
+    private func setupScrollView() {
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func setupImageView() {
+        scrollView.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFit
+    }
+    
+    private func setupButtons() {
+        backButton.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        backButton.tintColor = .white
+        backButton.titleLabel?.font = .systemFont(ofSize: 18)
+        backButton.addTarget(self, action: #selector(didTapBackButton), for: .touchUpInside)
+        
+        let shareBtnImg = UIImage(systemName: "square.and.arrow.down")
+        shareButton.setImage(shareBtnImg, for: .normal)
+        shareButton.tintColor = .white
+        shareButton.backgroundColor = .black
+        shareButton.clipsToBounds = true
+        shareButton.addTarget(self, action: #selector(didTapShareButton), for: .touchUpInside)
+        
+        [backButton, shareButton].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            backButton.heightAnchor.constraint(equalToConstant: 24),
+            backButton.widthAnchor.constraint(equalTo: backButton.heightAnchor),
+            
+            shareButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            shareButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            shareButton.heightAnchor.constraint(equalToConstant: 51),
+            shareButton.widthAnchor.constraint(equalToConstant: 51)
+        ])
+    }
+    
+    @objc private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc private func didTapShareButton() {
+        guard let image else { return }
+        let share = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        centerScrollViewContents()
+    }
+    
+    private func centerScrollViewContents() {
+        let boundsSize = scrollView.bounds.size
+        let contentsSize = scrollView.contentSize
+        
+        var contentInsets = UIEdgeInsets.zero
+        
+        if contentsSize.width < boundsSize.width {
+            contentInsets.left = (boundsSize.width - contentsSize.width) / 2
+            contentInsets.right = contentInsets.left
+        }
+        
+        if contentsSize.height < boundsSize.height {
+            contentInsets.top = (boundsSize.height - contentsSize.height) / 2
+            contentInsets.bottom = contentInsets.top
+        }
+        
+        scrollView.contentInset = contentInsets
+    }
+}
 
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        centerScrollViewContents()
+    }
+}
--- ImagesFeed/Profile/ProfileViewController.swift
+++ ImagesFeed/Profile/ProfileViewController.swift
@@ -1,2 +1,80 @@
 import UIKit
 
+final class ProfileViewController: UIViewController {
+    
+    // MARK: - UI Elements
+    private let profileImageView = UIImageView()
+    private let logoutButton = UIButton()
+    private let nameSurname = UILabel()
+    private let username = UILabel()
+    private let profileMessage = UILabel()
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - Setup
+    private func setupUI() {
+        view.backgroundColor = AppColors.background
+        setupSubviews()
+        setupConstraints()
+        configureViews()
+    }
+    
+    private func setupSubviews() {
+        [profileImageView, logoutButton, nameSurname, username, profileMessage].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            profileImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: AppLayout.spacingHorizontal),
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: AppLayout.profileTopSpacing),
+            profileImageView.heightAnchor.constraint(equalToConstant: AppLayout.avatarSize),
+            profileImageView.widthAnchor.constraint(equalToConstant: AppLayout.avatarSize),
+            
+            logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -AppLayout.spacingHorizontal),
+            logoutButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
+            logoutButton.heightAnchor.constraint(equalToConstant: AppLayout.logoutImageSize),
+            logoutButton.widthAnchor.constraint(equalToConstant: AppLayout.logoutImageSize),
+            
+            nameSurname.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+            nameSurname.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: AppLayout.spacingBetweenLines),
+            nameSurname.trailingAnchor.constraint(lessThanOrEqualTo: logoutButton.leadingAnchor, constant: -20),
+            
+            username.leadingAnchor.constraint(equalTo: nameSurname.leadingAnchor),
+            username.topAnchor.constraint(equalTo: nameSurname.bottomAnchor, constant: AppLayout.spacingBetweenLines),
+            username.trailingAnchor.constraint(lessThanOrEqualTo: logoutButton.trailingAnchor, constant: 20),
+            
+            profileMessage.leadingAnchor.constraint(equalTo: nameSurname.leadingAnchor),
+            profileMessage.topAnchor.constraint(equalTo: username.bottomAnchor, constant: AppLayout.spacingBetweenLines),
+            profileMessage.trailingAnchor.constraint(lessThanOrEqualTo: logoutButton.trailingAnchor, constant: 20),
+        ])
+    }
+    
+    private func configureViews() {
+        profileImageView.image = UIImage(resource: .avatar)
+        profileImageView.layer.cornerRadius = AppLayout.avatarSize / 2
+        profileImageView.layer.masksToBounds = true
+        profileImageView.contentMode = .scaleAspectFill
+        
+        logoutButton.setImage(UIImage(resource: .logoutButton), for: .normal)
+        
+        nameSurname.text = "Имя Фамилия"
+        nameSurname.font = AppFonts.title
+        nameSurname.textColor = AppColors.textPrimary
+        
+        username.text = "@asd"
+        username.font = AppFonts.caption
+        username.textColor = AppColors.textSecondary
+        
+        profileMessage.text = "Hello world!"
+        profileMessage.font = AppFonts.body
+        profileMessage.textColor = AppColors.textPrimary
+    }
+    
+}
